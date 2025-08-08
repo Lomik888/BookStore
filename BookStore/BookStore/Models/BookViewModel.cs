30715f2e158a5987428c2453720d678885b0c4c1
Solovev <solovtev17@bk.ru>
2025-08-08 13:05:39 +0300

    Добавил контроллер для книг и ViewModel

---
+++ BookStore/BookStore/Models/BookViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Models;
+
+public class BookViewModel
+{
+    public string Title { get; set; } = "";
+    public string Author { get; set; } = "";
+    public int PublishedYear { get; set; }
+}
